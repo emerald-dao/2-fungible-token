3c63065eb98ed2bb714404a5fe3675bce89acb45
Pelumi Oyesanya <pelumi351@gmail.com>
2022-05-26 05:05:51 +0100

    styled up

--- pages/index.js
+++ pages/index.js
@@ -120,22 +120,69 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <Head>
-        <title>2-FUNGIBLE-TOKEN</title>
-        <meta name="description" content="Used by Emerald Academy" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <h1>User Address: {user.loggedIn ? user.addr : null}</h1>
-      <button onClick={fcl.authenticate}>Log In</button>
-      <button onClick={fcl.unauthenticate}>Log Out</button>
-      <button onClick={setupVault}>Setup Vault</button>
-      <button onClick={getBalance}>Get Balance</button>
-      <input type="text" placeholder="recipient address" onChange={e => setRecipient(e.target.value)} />
-      <input type="text" placeholder="amount" onChange={e => setAmount(e.target.value)} />
-      <button onClick={() => transferTokens(amount, recipient)}>Transfer Tokens</button>
-      <h2>Balance: {balance}</h2>
+    <div className='bg-[#011E30] flex flex-col min-h-screen'>
+    <Head>
+      <title>2-SIMPLE-FT</title>
+      <meta name="description" content="Used by Emerald Academy" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main className='container mx-auto flex-1 p-5'>
+      <div className='mb-5 flex justify-between items-center pr-10 pt-2'>
+        <h1 className={styles.sooth}>SIMPLE-FT</h1>
+        <div className='flex space-x-4 items-center'>
+          <h1 className='text-[#38E8C6]'>Address: </h1>
+          <h1 className='border px-7 text-center text-[#38E8C6] text-sm py-1 rounded-xl border-[#38E8C6] w-56'>{user.loggedIn ? user.addr : "Please connect wallet -->"}</h1>
+        </div>
+        <div>{!user.loggedIn ? <button className='border rounded-xl border-[#38E8C6] px-5 text-sm text-[#38E8C6] py-1' 
+          onClick={fcl.authenticate}>connect</button> : <button className='border rounded-xl border-[#38E8C6]
+          px-5 text-sm text-[#38E8C6] py-1' onClick={fcl.unauthenticate}>Logout</button>}
+        </div>
+      </div>
+      <hr className='border-[#38E8C6]'/>
+      {!user.loggedIn ? '' :<div className='flex pt-5 px-20 justify-between'>
+        <button onClick={setupVault} className='rounded-lg px-9 py-2 text-blue-900 font-bold text-sm bg-[#38E8C6]'>Setup Vault</button>
+        <div className='flex items-center '>
+          <button className='text-[#38E8C6] text-lg px-2'>Balance :</button>
+          <h2 className='rounded-lg bg-[#00344B] text-gray-300 py-2 px-6'>{balance}</h2>
+          <svg xmlns="http://www.w3.org/2000/svg" onClick={getBalance} className="h-7 w-7 cursor-pointer pl-2" fill="none" viewBox="0 0 24 24" stroke="#38E8C6" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+          </svg>
+        </div>
+      </div>}
+      <div className='flex flex-col items-center text-white justify-center pt-28'>
+        {!user.loggedIn ? <div className='flex flex-col justify-center items-center'>
+              <img src='/whistle.svg' width={200} alt='nothing to see here'/>
+              <h1 className='pt-5 font-semibold text-gray-600'>Nothing to see here. Please connect your wallet</h1>
+            </div> 
+        :<div className='flex bg-gray-900 rounded-lg shadow-lg px-5 py-7 flex-col space-y-5 w-1/3'>
+          <div className='flex justify-between'>
+            <h1 className='text-lg font-semibold text-gray-100 mb-2'>Transfer Tokens</h1>
+            <img src='/planee.png' alt='plane'/>
+          </div>
+          <div className='flex flex-col'>
+            <label className='text-sm text-[#38E8C6]'>address</label>
+            <input type="text" placeholder="recipient address" 
+            className='px-4 py-2 focus:outline-none focus:border-[#38E8C6] focus:border-2 bg-gray-800 focus:border rounded-lg'
+            onChange={e => setRecipient(e.target.value)} />
+          </div>
+          <div className='flex flex-col pb-2'>
+            <label className='text-sm text-[#38E8C6]'>amount</label>
+            <input type="text" placeholder="amount" 
+            className='px-4 py-2 focus:outline-none focus:border-[#38E8C6] focus:border-2 bg-gray-800 focus:border rounded-lg'
+            onChange={e => setAmount(e.target.value)} />
+          </div>
+          <button onClick={() => transferTokens(amount, recipient)}
+          className='rounded-lg text-center text-sm font-bold text-blue-900 py-2 bg-[#38E8C6]'
+          >Transfer Tokens</button>
+        </div>}
+      </div>
+    </main>
+    <footer className='container mx-auto'>
+      <img src='/city.svg' alt='city'/>
+    </footer>
     </div>
+    
   )
 }
+
+
