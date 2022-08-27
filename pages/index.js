bfea1de7177f13f89f29890513421f1e26f252c2
BoiseITGuru <dev@boiseitguru.com>
2022-08-26 23:04:17 -0600

    fixed react property issues

--- pages/index.js
+++ pages/index.js
@@ -142,8 +142,8 @@ export default function Home() {
           <div className='flex items-center '>
             <button className='text-[#38E8C6] text-lg px-2'>Balance :</button>
             <h2 className='rounded-lg bg-[#00344B] text-gray-300 py-2 px-6'>{balance}</h2>
-            <svg xmlns="http://www.w3.org/2000/svg" onClick={getBalance} className="h-7 w-7 cursor-pointer pl-2" fill="none" viewBox="0 0 24 24" stroke="#38E8C6" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+            <svg xmlns="http://www.w3.org/2000/svg" onClick={getBalance} className="h-7 w-7 cursor-pointer pl-2" fill="none" viewBox="0 0 24 24" stroke="#38E8C6" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
             </svg>
           </div>
         </div>}
